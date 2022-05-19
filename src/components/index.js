dcde33e6a8413418f0ec91225abce461274c8893
Runar <runarnazmutdinov@gmail.com>
2022-05-19 23:37:08 +0300

    layout of hero, process, features, consult, video blocks

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import './header/header';
 import './footer/footer';
 import './hero/hero';
+import './process/process';
+import './features/features';
+import './consult/consult';
+import './video/video';
