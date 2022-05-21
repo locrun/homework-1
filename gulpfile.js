b8f0a6faca12141a06be8a696dc41b0d32bc225a
Runar <runarnazmutdinov@gmail.com>
2022-05-21 13:42:34 +0300

    photo gallery and footer layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 const { parallel, series } = require('gulp');
 
-const { views, clean, server, styles, images, fonts, scripts, svgSprite } = require('./gulp/tasks');
+const {
+  views,
+  clean,
+  server,
+  styles,
+  images,
+  fonts,
+  scripts,
+  svgSprite,
+} = require('./gulp/tasks');
 
-const build = series(clean, parallel(views, styles, scripts, fonts, images, svgSprite));
+const build = series(
+  clean,
+  parallel(views, styles, scripts, fonts, images, svgSprite)
+);
 const start = series(build, server);
 
 module.exports = {
   start,
   build,
-}
+};
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ import './process/process';
 import './features/features';
 import './consult/consult';
 import './video/video';
+import './gallery/gallery';
